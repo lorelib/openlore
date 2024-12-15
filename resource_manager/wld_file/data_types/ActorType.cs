c388262498be507ba6faf75913b41fd0b392896e
Pablo Alcubilla <anarelion@gmail.com>
2024-12-15 22:46:02 +0000

    Add missing files due to gitignore

---
+++ resource_manager/wld_file/data_types/ActorType.cs
@@ -0,0 +1,11 @@
+﻿namespace OpenLore.resource_manager.wld_file.data_types;
+
+// Lantern Extractor class
+public enum ActorType
+{
+    Camera = 0,
+    Static = 1,
+    Skeletal = 2,
+    Particle = 3,
+    Sprite = 4
+}
---
+++ resource_manager/wld_file/data_types/Animation.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using Godot;
+using OpenLore.resource_manager.wld_file.fragments;
+
+namespace OpenLore.resource_manager.wld_file.data_types;
+
+// Lantern Extractor class
+[GlobalClass]
+public partial class Animation : Resource
+{
+    public string AnimModelBase;
+    public int FrameCount;
+    public Dictionary<string, Frag13Track> Tracks;
+    public Dictionary<string, Frag13Track> TracksCleaned;
+    public Dictionary<string, Frag13Track> TracksCleanedStripped;
+
+    public Animation()
+    {
+        Tracks = [];
+        TracksCleaned = [];
+        TracksCleanedStripped = [];
+    }
+
+    public int AnimationTimeMs;
+
+    public static string CleanBoneName(string boneName)
+    {
+        if (string.IsNullOrEmpty(boneName)) return boneName;
+
+        var cleanedName = CleanBoneNameDag(boneName);
+        return cleanedName.Length == 0 ? "root" : cleanedName;
+    }
+
+    public static string CleanBoneAndStripBase(string boneName, string modelBase)
+    {
+        var cleanedName = CleanBoneNameDag(boneName);
+
+        if (cleanedName.StartsWith(modelBase)) cleanedName = cleanedName.Substring(modelBase.Length);
+
+        return cleanedName.Length == 0 ? "root" : cleanedName;
+    }
+
+    public static string CleanBoneNameDag(string boneName)
+    {
+        if (string.IsNullOrEmpty(boneName)) return boneName;
+
+        return boneName.ToLower().Replace("_dag", string.Empty);
+    }
+
+    public void AddTrack(Frag13Track track, string pieceName, string cleanedName, string cleanStrippedName)
+    {
+        // Prevent overwriting tracks
+        // Drachnid edge case
+        if (Tracks.ContainsKey(pieceName)) return;
+
+        Tracks[pieceName] = track;
+        TracksCleaned[cleanedName] = track;
+        TracksCleanedStripped[cleanStrippedName] = track;
+
+        if (string.IsNullOrEmpty(AnimModelBase) &&
+            !string.IsNullOrEmpty(track.ModelName))
+            AnimModelBase = track.ModelName;
+
+        if (track.TrackDefFragment.Frames.Count > FrameCount) FrameCount = track.TrackDefFragment.Frames.Count;
+
+        var totalTime = track.TrackDefFragment.Frames.Count * track.FrameMs;
+
+        if (totalTime > AnimationTimeMs) AnimationTimeMs = totalTime;
+    }
+}
