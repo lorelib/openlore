dff673e556ffae3feb17dd11a8f78fee92c6ac0c
anarelion <anarelion@gmail.com>
2024-12-15 23:52:38 +0100

    Rename variable to fix warning (#2)

--- resource_manager/wld_file/fragments/Frag2DDmSprite.cs
+++ resource_manager/wld_file/fragments/Frag2DDmSprite.cs
@@ -6,7 +6,7 @@ namespace OpenLore.resource_manager.wld_file.fragments;
 [GlobalClass]
 public partial class Frag2DDmSprite : WldFragment
 {
-    [Export] public int Reference;
+    [Export] public int MeshReference;
     [Export] public Frag36DmSpriteDef2 NewMesh;
     [Export] public Frag2CDmSpriteDef OldMesh;
 
@@ -14,8 +14,8 @@ public partial class Frag2DDmSprite : WldFragment
     {
         base.Initialize(index, type, size, data, wld, loader);
         Name = wld.GetName(Reader.ReadInt32());
-        Reference = Reader.ReadInt32();
-        var fragment = wld.GetFragment(Reference);
+        MeshReference = Reader.ReadInt32();
+        var fragment = wld.GetFragment(MeshReference);
 
         NewMesh = fragment as Frag36DmSpriteDef2;
         if (NewMesh != null) return;
